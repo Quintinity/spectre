5ac3c72b293b0c0088a9e727cfb030bc99c36444
Quintinity <vlad.marica1@gmail.com>
2020-01-26 10:54:16 -0400

    Update dependencies and gulpfile to use Dart Sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,67 +1,63 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var cleancss = require('gulp-clean-css');
-var csscomb = require('gulp-csscomb');
-var rename = require('gulp-rename');
-var autoprefixer = require('gulp-autoprefixer');
-var pug = require('gulp-pug');
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+sass.compiler = require('sass'); // use Dart Sass
+const cleancss = require('gulp-clean-css');
+const rename = require('gulp-rename');
+const autoprefixer = require('gulp-autoprefixer');
+const pug = require('gulp-pug');
 
-var paths = {
+const paths = {
   source: './src/*.scss',
   doc: './docs/src/scss/*.scss'
 };
 
-gulp.task('watch', function() {
-  gulp.watch('./**/*.scss', ['build']);
-  gulp.watch('./**/*.scss', ['docs']);
-  gulp.watch('./**/*.pug', ['docs']);
-});
+async function _build(filePaths, destination) {
+  return new Promise((resolve, reject) => {
+    gulp.src(filePaths)
+      .pipe(sass({ outputStyle: 'expanded', precision: 10 })
+        .on('error', sass.logError)
+      )
+      .pipe(autoprefixer())
+      .pipe(gulp.dest(destination))
+      .pipe(cleancss())
+      .pipe(rename({
+        suffix: '.min'
+      }))
+      .pipe(gulp.dest(destination))
+      .on('end', resolve)
+      .on('error', reject);
+  });
+}
 
-gulp.task('build', function() {
-  gulp.src(paths.source)
-    .pipe(sass({outputStyle: 'compact', precision: 10})
-      .on('error', sass.logError)
-    )
-    .pipe(autoprefixer())
-    .pipe(csscomb())
-    .pipe(gulp.dest('./dist'))
-    .pipe(cleancss())
-    .pipe(rename({
-      suffix: '.min'
-    }))
-    .pipe(gulp.dest('./dist'));
-});
+async function build() {
+  return _build(paths.source, './dist');
+}
 
-gulp.task('docs', function() {
-  gulp.src(paths.doc)
-    .pipe(sass({outputStyle: 'compact', precision: 10})
-      .on('error', sass.logError)
-    )
-    .pipe(autoprefixer())
-    .pipe(csscomb())
-    .pipe(gulp.dest('./docs/dist'))
-    .pipe(cleancss())
-    .pipe(rename({
-      suffix: '.min'
-    }))
-    .pipe(gulp.dest('./docs/dist'));
-  gulp.src(paths.source)
-    .pipe(sass({outputStyle: 'compact', precision: 10})
-      .on('error', sass.logError)
-    )
-    .pipe(autoprefixer())
-    .pipe(csscomb())
-    .pipe(gulp.dest('./docs/dist'))
-    .pipe(cleancss())
-    .pipe(rename({
-      suffix: '.min'
-    }))
-    .pipe(gulp.dest('./docs/dist'));
-  gulp.src('docs/src/**/!(_)*.pug')
-    .pipe(pug({
-      pretty: true
-    }))
-    .pipe(gulp.dest('./docs/'));
-});
+async function docs() {
+  const pugTask = new Promise((resolve, reject) => {
+    gulp.src('docs/src/**/!(_)*.pug')
+      .pipe(pug({
+        pretty: true
+      }))
+      .pipe(gulp.dest('./docs/'))
+      .on('end', resolve)
+      .on('error', reject);
+  });
+  
+  return Promise.all([
+    _build(paths.doc, './docs/dist'),
+    _build(paths.source, './docs/dist'),
+    pugTask
+  ]);
+}
 
-gulp.task('default', ['build']);
+function watch() {
+  gulp.watch('./**/*.scss', build);
+  gulp.watch('./**/*.scss', docs);
+  gulp.watch('./**/*.pug', docs);
+}
+
+exports.build = build;
+exports.docs = docs;
+exports.watch = watch;
+exports.default = build;
